9c5e83e7a6104687d8bba6e13f89dd5a495def8b
jinuman <jin35kim@gmail.com>
2018-04-28 21:10:36 +0900

    Connection to mongodb setting (using mongoose, mongoose-auto-increment)

--- app.js
+++ app.js
@@ -3,6 +3,28 @@ let path = require('path');
 let logger = require('morgan');
 let bodyParser = require('body-parser');
 
+/**
+ * DB setting
+ * */
+// mongodb connect
+let mongoose = require('mongoose');
+let autoIncrement = require('mongoose-auto-increment');
+mongoose.Promise = global.Promise;
+
+// DB collection: create secondhandshop collection and connect
+let databaseUrl = 'mongodb://localhost:27017/secondhandshop';
+let connect = mongoose.connect(databaseUrl,
+    { useMongoClient: true }
+);
+// automatically increase primary key
+autoIncrement.initialize(connect);
+
+let db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function () {
+    console.log('mongodb connection success!');
+});
+
 let app = express();
 let port = 3000;        // port number
 
