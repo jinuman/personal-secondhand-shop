3f5dddb4f4078e4b29fd70786232ef5e2fff05a8
jinuman <jin35kim@gmail.com>
2018-04-29 00:53:15 +0900

    Set ProductsSchema and export

---
+++ models/ProductsModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const autoIncrement = require('mongoose-auto-increment');
+
+// Create Fields
+const ProductsSchema = new Schema({
+    name: String,           // 제품 이름
+    price: Number,          // 제품 가격
+    description: String,    // 제품 설명
+    created_at: {           // 작성한 날
+        type: Date,
+        default: Date.now()
+    }
+});
+
+// virtual model: collection 에서 정의되지 않은 field 이지만 정의된 field 처럼 사용할 수 있게 하는 기술
+// virtual 변수는 호출되면 실행하는 함수
+// set 은 변수의 값을 바꾸거나 셋팅하면 호출
+// getDate 변수를 호출하는 순간 년, 월, 일이 찍힌다.
+ProductsSchema.virtual('getDate').get(() => {   // getDate 필드가 생긴다.
+    let date = new Date(this.created_at);
+    return {
+        year: date.getFullYear(),
+        month: date.getMonth() + 1,
+        day: date.getDate()
+    };
+});
+
+// model: Collection name, field: primary key, startAt: 1부터 시작
+// primary key will increment by 1 for each new record.
+ProductsSchema.plugin(autoIncrement.plugin, {model: 'products', field: 'id', startAt: 1});
+module.exports = mongoose.model('products', ProductsSchema);
+
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const ProductsModel = require('../models/ProductsModel');
 
 // admin page
 router.get('/', function (req, res) {
